c2ed2859147be40a7942da77bd9bc1c3ad62ea7f
squireChuck <squirechucks.codequest@gmail.com>
2019-01-10 22:01:16 -0500

    solution to prob 1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('first msg')
+const sum = [...Array(1000).keys()].reduce((accumulator, currentValue, idx) => {
+  const isMultipleOf3Or5 = (numberToCheck: number) =>
+    numberToCheck % 3 === 0 || numberToCheck % 5 === 0;
+  accumulator += isMultipleOf3Or5(idx) ? idx : 0;
+  return accumulator;
+}, 0);
+
+console.log(`Sum is... ${sum}`);
